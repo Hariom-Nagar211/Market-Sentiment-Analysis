673fa3497c8de02a6e2f09b03f8334aa62ac5175
Hariom Nagar <bhandarihariom211@gmail.com>
2026-05-21 23:41:55 +0530

    Update market_sentiment_analysis.py

--- market_sentiment_analysis.py
+++ market_sentiment_analysis.py
@@ -1,15 +1,15 @@
-import pandas as pd
+ pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Data Preparation
-def load_data(file_path):
+df load_data(file_path):
     data = pd.read_csv(file_path)
     data['Date'] = pd.to_datetime(data['Date'])
     data.set_index('Date', inplace=True)
-    return data
+    retn data
 
 def calculate_returns(data):
     data['Daily_Return'] = data['Close'].pct_change()
